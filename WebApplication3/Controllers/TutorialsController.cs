1d6ef7b8a0e827bf016360f83411eab4ecd1a1d7
pupsikdhd <128391561+pupsikdhd@users.noreply.github.com>
2024-08-01 14:59:21 +0700

    1.1

---
+++ WebApplication3/Controllers/TutorialsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class TutorialsController : Controller
+    {
+        public ActionResult t1()
+        {
+            return View();
+        }
+        public ActionResult t2()
+        {
+            return View();
+        }
+    }
+}
